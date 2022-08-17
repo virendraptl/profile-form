85245525401148fe57b94f6b2fc06fe7e77a7a19
virendraptl <virendra.patil253@gmail.com>
2022-08-17 09:37:46 +0530

    checkout address CRUD complete

--- src/app/modules/customer/cart/checkout/checkout.component.ts
+++ src/app/modules/customer/cart/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
 import Stepper from 'bs-stepper';
 import { HeaderTitleService } from 'src/app/services/header-title/header-title.service';
 import { HttpService } from 'src/app/services/http/http.service';
@@ -22,12 +24,26 @@ export class CheckoutComponent implements OnInit {
   finalAddress: any;
   cartProducts;
   subTotal: number;
+  newAddressForm: FormGroup;
+
+  editOn: boolean = false;
+  editFormNo: number;
+  editAddId: string;
+  emptyAddrForm = {
+    street: '',
+    addressLine2: '',
+    city: '',
+    state: '',
+    pin: '',
+  };
 
   constructor(
     private headerTitleService: HeaderTitleService,
     public previousRouteService: PreviousRouteService,
     private lstore: LocalStorageService,
+    private fb: FormBuilder,
     private router: Router,
+    private toasterService: HotToastService,
     private http: HttpService
   ) {
     this.headerTitleService.setTitle('Checkout');
@@ -56,6 +72,7 @@ export class CheckoutComponent implements OnInit {
       linear: true,
       animation: true,
     });
+    this.createNewAdrForm(this.emptyAddrForm);
   }
 
   getProfileData(token) {
@@ -87,6 +104,73 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
+  editAddress(i: number, id) {
+    this.editFormNo = i;
+    this.editAddId = id;
+    this.editOn = true;
+    this.createNewAdrForm(this.addressList[i]);
+  }
+
+  emptyAddress() {
+    this.editOn = false;
+    this.createNewAdrForm(this.emptyAddrForm);
+  }
+
+  createNewAdrForm(address) {
+    this.newAddressForm = this.fb.group({
+      street: [address.street, [Validators.required]],
+      addressLine2: [address.addressLine2, [Validators.required]],
+      city: [address.city, [Validators.required]],
+      state: [address.state, [Validators.required]],
+      pin: [address.pin, [Validators.required]],
+    });
+  }
+
+  submitAddrForm() {
+    if (!this.editOn) {
+      console.log('new address:', this.newAddressForm.value);
+      this.http
+        .postSecured(
+          'customers/address',
+          this.newAddressForm.value,
+          this.userToken
+        )
+        .subscribe({
+          next: (data) => {
+            this.toasterService.success(`"Address list updated!`);
+
+            this.getAddresses();
+          },
+          error: (error) => {
+            console.error('Error in fetching address list:', error.message);
+          },
+        });
+    } else {
+      this.editOn = false;
+      console.log(
+        'updated address at',
+        this.editFormNo,
+        this.newAddressForm.value
+      );
+      this.http
+        .putSecured(
+          `customers/address/${this.editAddId}`,
+          this.newAddressForm.value,
+          this.userToken
+        )
+        .subscribe({
+          next: (data) => {
+            this.toasterService.success(`"Address list updated!`);
+
+            this.getAddresses();
+          },
+          error: (error) => {
+            console.error('Error in fetching address list:', error.message);
+          },
+        });
+    }
+  }
+
   next() {
     this.stepCount += 1;
     this.stepper.next();
@@ -104,6 +188,22 @@ export class CheckoutComponent implements OnInit {
 
   radioOp() {
     console.log(this.finalAddress);
+  }
+
+  get regAdStreet() {
+    return this.newAddressForm.get('street');
+  }
+  get regAdLine2() {
+    return this.newAddressForm.get('addressLine2');
+  }
+  get regAdCity() {
+    return this.newAddressForm.get('city');
+  }
+  get regAdState() {
+    return this.newAddressForm.get('state');
+  }
+  get regAdPin() {
+    return this.newAddressForm.get('pin');
   }
 }
 
